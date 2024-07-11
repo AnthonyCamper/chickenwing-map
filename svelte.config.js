5501b85a76fa675e87c47cb3fdb23ba845319f4d
anthony <33391769+AnthonyCamper@users.noreply.github.com>
2024-07-10 23:20:31 -0400

    final update before deploy + clean

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-	  adapter: adapter(),
-	  paths: {
-		base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
-	  },
-	}
-  };
-  
-  export default config;
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/chickenwing-map' : '',
+    },
+  }
+};
+
+export default config;
