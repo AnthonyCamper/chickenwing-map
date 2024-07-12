8385b271e3684817d5634aeb609cc82966693cef
anthony <33391769+AnthonyCamper@users.noreply.github.com>
2024-07-11 22:30:48 -0400

    please be the one

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
   kit: {
-      adapter: adapter({
-        pages: 'build',
-        assets: 'build',
-        fallback: '404.html',
-        precompress: false,
-        strict: true
-  }),
-      paths: {
-        base: process.env.NODE_ENV === 'production' ? '/chickenwing-map' : '',
-      }
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: dev ? '' : '',
+    }
   }
 };
+
 export default config;
