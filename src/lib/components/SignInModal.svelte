825cd6aa0bf944a27ebf85aed8e4784a64473efd
HappyCamper <33391769+AnthonyCamper@users.noreply.github.com>
2024-12-11 20:21:38 -0500

    fixed z axis on sign in modal

--- src/lib/components/SignInModal.svelte
+++ src/lib/components/SignInModal.svelte
@@ -52,7 +52,7 @@
 </script>
 
 {#if show}
-  <div class="fixed inset-0 bg-black bg-opacity-50 z-[3000] flex items-center justify-center p-4">
+  <div class="fixed inset-0 bg-black bg-opacity-50 z-[10000] flex items-center justify-center p-4">
     <div 
       transition:fly={{ y: 20, duration: 300 }}
       class="bg-white dark:bg-gray-800 rounded-lg shadow-xl max-w-md w-full p-6"
